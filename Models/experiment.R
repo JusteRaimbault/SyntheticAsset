aebca1e399cf644a75748620e88770d079bbd662
JusteRaimbault <raimbaultjwin@gmail.com>
2016-01-08 19:16:35 +0100

    correc_sing_arma

--- Models/experiment.R
+++ Models/experiment.R
@@ -6,10 +6,10 @@
 
 resdir = 'res/'
 assets = c("EURUSD","EURGBP")
-#months = paste0("2015",c("06","07","08","09","10","11"))
-months=c("201506")
-#filterings = c(3,6,12,24)
-filterings=c(24)
+months = paste0("2015",c("06","07","08","09","10","11"))
+#months=c("201506")
+filterings = c(3,6,12)
+#filterings=c(12)
 correlations = seq(from=0,to=0.95,by=0.05)
 omega0 = 72
 
@@ -29,9 +29,9 @@ for(month in months){
     # refilter and resample
     x1 = sample(gaussianFilter(x1,filtering),filtering);x2 = sample(gaussianFilter(x2,filtering),filtering)
     # generate synthetic data for each level of correlation
-    synthlength = length(x1)-4*omega0
+    synthlength = length(x1)-(4*omega0*3/filtering)
     synth = matrix(0,2*synthlength,length(correlations))
-    for(i in 1:length(correlations)){s=synthAssets(x1,x2,correlations[i],omega0);synth[,i]=c(s[,1],s[,2])}
+    for(i in 1:length(correlations)){s=synthAssets(x1,x2,correlations[i],omega0*3/filtering);synth[,i]=c(s[,1],s[,2])}
     
     # compute perfs in //
     res <- foreach(j=1:ncol(synth)) %dopar% {
